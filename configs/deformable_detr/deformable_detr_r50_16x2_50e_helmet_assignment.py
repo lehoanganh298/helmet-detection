a50ce7fc773778cd7200b9cbc303ffe50315384e
lehoanganh298 <lehoanganh29896@gmail.com>
2021-10-15 23:11:29 +0700

    Add config files for HelmetDetection and HelmetAssignment

---
+++ configs/deformable_detr/deformable_detr_r50_16x2_50e_helmet_assignment.py
@@ -0,0 +1,101 @@
+_base_ = [
+    '../_base_/models/deformable_detr_r50_16x2_50e.py',
+    '../_base_/datasets/helmet_assignment.py', 
+    '../_base_/default_runtime.py'
+]
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
+# from the default setting in mmdet.
+train_pipeline = [
+    dict(type='LoadImageWithTrackingData'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='AutoAugment',
+        policies=[
+            [
+                dict(
+                    type='Resize',
+                    img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                               (576, 1333), (608, 1333), (640, 1333),
+                               (672, 1333), (704, 1333), (736, 1333),
+                               (768, 1333), (800, 1333)],
+                    multiscale_mode='value',
+                    keep_ratio=True)
+            ],
+            [
+                dict(
+                    type='Resize',
+                    # The radio of all image in train dataset < 7
+                    # follow the original impl
+                    img_scale=[(400, 4200), (500, 4200), (600, 4200)],
+                    multiscale_mode='value',
+                    keep_ratio=True),
+                dict(
+                    type='RandomCrop',
+                    crop_type='absolute_range',
+                    crop_size=(384, 600),
+                    allow_negative_crop=True),
+                dict(
+                    type='Resize',
+                    img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                               (576, 1333), (608, 1333), (640, 1333),
+                               (672, 1333), (704, 1333), (736, 1333),
+                               (768, 1333), (800, 1333)],
+                    multiscale_mode='value',
+                    override=True,
+                    keep_ratio=True)
+            ]
+        ]),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=1),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+# test_pipeline, NOTE the Pad's size_divisor is different from the default
+# setting (size_divisor=32). While there is little effect on the performance
+# whether we use the default setting or use size_divisor=1.
+test_pipeline = [
+    dict(type='LoadImageWithTrackingData'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=1),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(filter_empty_gt=False, pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+
+load_from ='/mnt/d/personal/kaggle/helmet_detection/codes/mmdetection/checkpoints/deformable_detr_r50_16x2_50e_helmet_detection.pth'
+
+model = dict(
+    bbox_head=dict(
+        type='DeformableDETRHeadWithTrackingData',
+        num_classes=1,
+    ),
+    test_cfg=dict(max_per_img=22)
+)
+# Set up working dir to save files and logs.
+work_dir = './log_defomable_detr'
+
+# The original learning rate (LR) is set for 8-GPU training.
+# We divide it by 8 since we only use one GPU.
+optimizer=dict(lr = 1e-5)
+# Change the evaluation metric since we use customized dataset.
+# We can set the evaluation interval to reduce the evaluation times
+evaluation=dict(metric = 'mAP',interval = 1)
+# We can set the checkpoint saving interval to reduce the storage cost
+checkpoint_config=dict(interval = 1)
