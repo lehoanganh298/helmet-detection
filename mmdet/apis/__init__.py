fbc687dce504e5ab5b251674227622085c0b8a96
lehoanganh298 <lehoanganh29896@gmail.com>
2021-10-15 22:43:38 +0700

    Add script to train on cpu Can't run entirely, only for debugging

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -3,9 +3,10 @@ from .inference import (async_inference_detector, inference_detector,
                         init_detector, show_result_pyplot)
 from .test import multi_gpu_test, single_gpu_test
 from .train import get_root_logger, set_random_seed, train_detector
+from .train_cpu import train_detector_cpu
 
 __all__ = [
-    'get_root_logger', 'set_random_seed', 'train_detector', 'init_detector',
+    'get_root_logger', 'set_random_seed', 'train_detector', 'train_detector_cpu', 'init_detector',
     'async_inference_detector', 'inference_detector', 'show_result_pyplot',
     'multi_gpu_test', 'single_gpu_test'
 ]
