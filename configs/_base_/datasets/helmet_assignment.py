9a942a601f1794b3a1ad4127c4fe9b5af4b90981
lehoanganh298 <lehoanganh29896@gmail.com>
2021-10-16 15:57:54 +0700

    Update data folder config

--- configs/_base_/datasets/helmet_assignment.py
+++ configs/_base_/datasets/helmet_assignment.py
@@ -1,24 +1,24 @@
 _base_ = 'coco_detection.py'
 
 dataset_type = 'HelmetAssignmentDataset'
-data_root = 'data/'
+data_root = 'dataset/'
 
 data = dict(
     train=dict(
         type='HelmetAssignmentDataset',
-        ann_file='data/dataset/labels_3_videos.jsonl',
-        img_prefix='data/dataset/train_frames_3_videos/',
-        tracking_prefix='data/dataset/train_tracking_3_videos'
+        ann_file='dataset/labels_3_videos.jsonl',
+        img_prefix='dataset/train_frames_3_videos/',
+        tracking_prefix='dataset/train_tracking_3_videos'
         ),
     val=dict(
         type='HelmetAssignmentDataset',
-        ann_file='data/dataset/labels_3_videos.jsonl',
-        img_prefix='data/dataset/train_frames_3_videos/',
-        tracking_prefix='data/dataset/train_tracking_3_videos'
+        ann_file='dataset/labels_3_videos.jsonl',
+        img_prefix='dataset/train_frames_3_videos/',
+        tracking_prefix='dataset/train_tracking_3_videos'
         ),
     test=dict(
         type='HelmetAssignmentDataset',
-        ann_file='data/dataset/labels_3_videos.jsonl',
-        img_prefix='data/dataset/train_frames_3_videos/',
-        tracking_prefix='data/dataset/train_tracking_3_videos'
+        ann_file='dataset/labels_3_videos.jsonl',
+        img_prefix='dataset/train_frames_3_videos/',
+        tracking_prefix='dataset/train_tracking_3_videos'
         ))
--- configs/_base_/datasets/helmet_detection.py
+++ configs/_base_/datasets/helmet_detection.py
@@ -1,18 +1,18 @@
 _base_ = 'coco_detection.py'
 
 dataset_type = 'HelmetDataset'
-data_root = 'data/'
+data_root = 'dataset/'
 
 data = dict(
     train=dict(
         type='HelmetDataset',
-        ann_file='data/train_dataset.json',
-        img_prefix='data/images'),
+        ann_file='dataset/train_dataset.json',
+        img_prefix='dataset/images'),
     val=dict(
         type='HelmetDataset',
-        ann_file='data/val_dataset.json',
-        img_prefix='data/images'),
+        ann_file='dataset/val_dataset.json',
+        img_prefix='dataset/images'),
     test=dict(
         type='HelmetDataset',
-        ann_file='data/test_dataset.json',
-        img_prefix='data/images'))
+        ann_file='dataset/test_dataset.json',
+        img_prefix='dataset/images'))
